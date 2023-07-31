f0ec31145be028ce121140580d564ee4c05c1a8c
mlswijerathne <136619138+mlswijerathne@users.noreply.github.com>
2023-07-31 16:18:04 +0530

    Add files via upload

---
+++ SE101.3-OOPJava-Lab-28576/Lab1_prac01Q02/src/main/java/org/example/Mavenproject12.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.mavenproject12;
+public class Mavenproject12 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Lakshitha Wijerathne");
+        System.out.println("Computer science");
+    }
+}
